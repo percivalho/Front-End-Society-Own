5c6b7b5e7d27a350ce5c6aafc479e28297ca4f7f
percivalho <35653644+percivalho@users.noreply.github.com>
2023-07-18 15:02:02 +0100

    debug message on login sound

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let sound = window.sound;
     let soundSrc = './assets/sound/' + sound;
-
+    console.log(window.soundPlay);
+    console.log(soundSrc);
     if (window.soundPlay <= 1) {
         // Create a new Howl instance
         let soundToPlay = new Howl({
