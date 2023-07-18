55d695a4489bb45a5c54b2dceda5e8376c289695
percivalho <35653644+percivalho@users.noreply.github.com>
2023-07-18 14:03:36 +0100

    fix lots of bugs

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
     let sound = window.sound;
     let soundSrc = './assets/sound/' + sound;
-    // Create a new Howl instance
-    let soundToPlay = new Howl({
-        src: [soundSrc],
-        autoplay: true 
-    });
-    message = soundSrc;
-    console.log(message);
-    
-    // Play the sound
-    soundToPlay.play();
+
+    if (window.soundPlay <= 1) {
+        // Create a new Howl instance
+        let soundToPlay = new Howl({
+            src: [soundSrc],
+            autoplay: true 
+        });
+        message = soundSrc;
+        console.log(message);
+        
+        // Play the sound
+        soundToPlay.play();
+    }
 });
   
